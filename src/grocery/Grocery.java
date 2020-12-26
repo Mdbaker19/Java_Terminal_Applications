3eaae61ed2f9e4809c24efefc641aedec79b6523
Matthew <matthew.d.baker20@gmail.com>
2020-12-26 09:39:50 -0700

    working on grocery app from class assignment with cleaner code

--- src/grocery/Grocery.java
+++ src/grocery/Grocery.java
@@ -3,9 +3,23 @@ package grocery;
 public class Grocery {
 
     private String itemName;
+    private String category;
+    private double price;
 
-    public Grocery(String name){
+    public Grocery(String name, String category){
         this.itemName = name;
+        this.category = category;
     }
-
+    public void setPrice(){
+        this.price = (Math.random() * 11) + 1;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public String getItemName(){
+        return this.itemName;
+    }
+    public String getCategory(){
+        return this.category;
+    }
 }
--- src/grocery/GroceryApplication.java
+++ src/grocery/GroceryApplication.java
@@ -1,4 +1,50 @@
 package grocery;
+import util.Input;
+
+import java.util.ArrayList;
 
 public class GroceryApplication {
+
+    public static void main(String[] args) {
+        Input sc = new Input();
+        ArrayList<Grocery> Cart = new ArrayList<>();
+        ArrayList<String> Categories = new ArrayList<>();
+
+        Grocery item1 = new Grocery("apple", "produce");
+        Cart.add(item1);
+        Categories.add(item1.getCategory());
+
+        Grocery item2 = new Grocery("steak", "meat");
+        Cart.add(item2);
+        Categories.add(item2.getCategory());
+
+
+        String one = "Item Name";
+        String two = "Category";
+        System.out.printf("%-10s|%-10s%n", one, two);
+        for(int i = 0; i < Cart.size(); i++){
+            format(Cart.get(i).getItemName(), Categories.get(i));
+        }
+
+        boolean notExit = true;
+        do{
+            int choice = sc.number(shop(), 1, 3);
+            if(choice == 3){
+                notExit = false;
+                System.out.println("Total is x");
+            }
+
+        }while(notExit);
+
+
+    }
+
+    public static String shop(){
+        return ("[1] Add to cart, [2] View cart, [3] Checkout");
+    }
+
+    public static void format(String s1, String s2){
+        System.out.printf("%-10s|%-10s%n", s1, s2);
+    }
+
 }
